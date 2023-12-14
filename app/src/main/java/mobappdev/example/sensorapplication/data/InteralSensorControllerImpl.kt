f1d668e8bcfef9a758f75d829dbcdb306ad07a4b
CaffeQ <102230900+CaffeQ@users.noreply.github.com>
2023-12-14 14:24:14 +0100

    able to run gyro and linAcc at same time

--- app/src/main/java/mobappdev/example/sensorapplication/data/InteralSensorControllerImpl.kt
+++ app/src/main/java/mobappdev/example/sensorapplication/data/InteralSensorControllerImpl.kt
@@ -24,6 +24,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import mobappdev.example.sensorapplication.data.Repository.MeasurementsRepo
+import mobappdev.example.sensorapplication.data.model.MathFilter
 import mobappdev.example.sensorapplication.data.model.Measurement
 import mobappdev.example.sensorapplication.data.model.addMeasurement
 import mobappdev.example.sensorapplication.domain.InternalSensorController
@@ -67,12 +68,6 @@ class InternalSensorControllerImpl(
 
     private var _currentMeasurements = MutableStateFlow<List<Measurement>>(emptyList())
 
-
-
-    private var _xPrevAngle = 0.0
-    private var _yPrevAngle = 0.0
-    private var _zPrevAngle = 0.0
-
     private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     private val gyroSensor: Sensor? by lazy {
         sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
@@ -80,6 +75,12 @@ class InternalSensorControllerImpl(
     private val linAccSensor: Sensor? by lazy {
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
+
+    private var lastLinAccSample: Triple<Float, Float, Float>? = null
+    private var lastGyroSample: Triple<Float, Float, Float>? = null
+
+    private lateinit var mathFilter: MathFilter
+
 
     override fun startImuStream() {
         if (linAccSensor == null) {
@@ -93,6 +94,7 @@ class InternalSensorControllerImpl(
 
         // Register this class as a listener for gyroscope events
         sensorManager.registerListener(this, linAccSensor, SensorManager.SENSOR_DELAY_UI)
+        mathFilter = MathFilter()
 
         // Start a coroutine to update the UI variable on a 2 Hz interval
         GlobalScope.launch(Dispatchers.Main) {
@@ -128,25 +130,35 @@ class InternalSensorControllerImpl(
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun startGyroStream() {
-        if (gyroSensor == null) {
-            Log.e(LOG_TAG, "Gyroscope sensor is not available on this device")
+        if (gyroSensor == null && linAccSensor == null) {
+            Log.e(LOG_TAG, "Gyroscope and LinAcc sensor is not available on this device")
             return
         }
-        if (_streamingGyro.value) {
-            Log.e(LOG_TAG, "Gyroscope sensor is already streaming")
+        if (_streamingGyro.value && _streamingLinAcc.value) {
+            Log.e(LOG_TAG, "Gyroscope and LinAcc sensor is already streaming")
             return
         }
 
         // Register this class as a listener for gyroscope events
+        sensorManager.registerListener(this, linAccSensor, SensorManager.SENSOR_DELAY_UI)
         sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_UI)
-
+        mathFilter = MathFilter()
         // Start a coroutine to update the UI variable on a 2 Hz interval
         GlobalScope.launch(Dispatchers.Main) {
             _streamingGyro.value = true
-            while (_streamingGyro.value) {
-                // Update the UI variable
-                _currentGyroUI.update { _currentGyro }
+            _streamingLinAcc.value = true
+            while (_streamingGyro.value && _streamingLinAcc.value) {
+                measurementsRepo.listOfMeasurementsFlow.collect { listOfMeasurements ->
+                    Log.d("COLLECTING","Size of listOfMeasurements: ${listOfMeasurements.size}")
+                    Log.d("COLLECTING","listOfMeasurements: $listOfMeasurements")
+                }
+                _currentMeasurements.addMeasurement(_currentLinAcc)
+                Log.d("MEASUREMENT", "Size=${_currentMeasurements.value.size}")
+                Log.d("MEASUREMENT", "Measurement=${_currentMeasurements.value.last()}")
 
+                _measurementsUI.update { _currentMeasurements.value }
+                _currentLinAccUI.update { _currentLinAcc }
+                _currentGyroUI.update { _currentGyro }
                 delay(500)
             }
         }
@@ -157,59 +169,31 @@ class InternalSensorControllerImpl(
         if (_streamingGyro.value) {
             // Unregister the listener to stop receiving gyroscope events (automatically stops the coroutine as well
             sensorManager.unregisterListener(this, gyroSensor)
+            sensorManager.unregisterListener(this, linAccSensor)
             _streamingGyro.value = false
         }
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
-            // Extract gyro data (angular speed around X, Y, and Z axes
-            val v = Triple(event.values[0], event.values[1], event.values[2])
-            _currentGyro = calculateAngles(v)
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            val linAccSample = Triple(event.values[0], event.values[1], event.values[2])
+            lastLinAccSample = linAccSample
+            _currentLinAcc = mathFilter.calculateAngles(linAccSample)
+            if (lastGyroSample != null) {
+                _currentGyro = mathFilter.calculateAnglesWithGyro(linAccSample, lastGyroSample!!)
+                _currentLinAcc = mathFilter.calculateAngles(linAccSample)
+            }
+        } else if (event.sensor.type == Sensor.TYPE_GYROSCOPE) {
+            val gyroSample = Triple(event.values[0], event.values[1], event.values[2])
+            lastGyroSample = gyroSample
 
-        }else if(event.sensor.type == Sensor.TYPE_ACCELEROMETER){
-            val v = Triple(event.values[0], event.values[1], event.values[2])
-            _currentLinAcc = calculateAngles(v)
+            if (lastLinAccSample != null) {
+                _currentGyro = mathFilter.calculateAnglesWithGyro(lastLinAccSample!!, gyroSample)
+                _currentLinAcc = mathFilter.calculateAngles(lastLinAccSample!!)
+            }
         }
     }
 
-    private fun calculateAngles(v:Triple<Float,Float,Float>):Triple<Float,Float,Float>{
-        val xAngleRaw = atan2(v.second, v.third) * (360 / Math.PI)
-        val yAngleRaw = atan2(v.first, v.third) * (360 / Math.PI)
-        val zAngleRaw = atan2(sqrt(v.first * v.first + v.second * v.second), v.third) * (180 / Math.PI)
-
-        val xAngle = filterEWMA(xAngleRaw,_xPrevAngle)
-        val yAngle = filterEWMA(yAngleRaw,_yPrevAngle)
-        val zAngle = filterEWMA(zAngleRaw,_zPrevAngle)
-
-        //Log.d("X_ANGLE","X-Angle=${xAngle}")
-        //Log.d("Y_ANGLE","Y-Angle=${yAngle}")
-        //Log.d("Z_ANGLE","Z-Angle=${zAngle}")
-
-        return Triple(xAngle.toFloat(), yAngle.toFloat(), zAngle.toFloat())
-    }
-
-    private fun calculateAngle(v:Triple<Float, Float, Float>):Double{
-        val refVector = Triple(1.0,0.0,0.0)
-
-        val dotProduct = v.first * refVector.first + v.second * refVector.second + v.third * refVector.third
-        val magnitudeRef =  sqrt(refVector.first*refVector.first + refVector.second * refVector.second + refVector.third * refVector.third)
-        val magnitudeVector = sqrt(v.first*v.first + v.second * v.second + v.third * v.third)
-
-        val cosTheta = dotProduct / (magnitudeRef * magnitudeVector)
-        val safeCosTheta = if (cosTheta > 1.0) 1.0 else if (cosTheta < -1.0) -1.0 else cosTheta
-
-        return atan(safeCosTheta)
-    }
-
-
-
-
-
-    private fun filterEWMA(value:Double,prevValue:Double):Double{
-        val a = 0.5
-        return a * value + (1 - a) * prevValue
-    }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
         // Not used in this example
---
+++ app/src/main/java/mobappdev/example/sensorapplication/data/model/MathFilter.kt
@@ -0,0 +1,50 @@
+package mobappdev.example.sensorapplication.data.model
+
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+class MathFilter {
+
+    private val alpha: Float = 0.5f
+
+    private var _xPrevAngle = 0.0
+    private var _yPrevAngle = 0.0
+    private var _zPrevAngle = 0.0
+    fun calculateAngles(v:Triple<Float,Float,Float>):Triple<Float,Float,Float>{
+        val xAngleRaw = atan2(v.second, v.third) * (360 / Math.PI)
+        val yAngleRaw = atan2(v.first, v.third) * (360 / Math.PI)
+        val zAngleRaw = atan2(sqrt(v.first * v.first + v.second * v.second), v.third) * (180 / Math.PI)
+
+        val xAngle = filterEWMA(xAngleRaw,_xPrevAngle)
+        val yAngle = filterEWMA(yAngleRaw,_yPrevAngle)
+        val zAngle = filterEWMA(zAngleRaw,_zPrevAngle)
+
+        //Log.d("X_ANGLE","X-Angle=${xAngle}")
+        //Log.d("Y_ANGLE","Y-Angle=${yAngle}")
+        //Log.d("Z_ANGLE","Z-Angle=${zAngle}")
+
+        return Triple(xAngle.toFloat(), yAngle.toFloat(), zAngle.toFloat())
+    }
+    fun calculateAnglesWithGyro(linSample:Triple<Float,Float,Float>,gyroSample:Triple<Float,Float,Float>):Triple<Float,Float,Float>{
+        val xAngleRaw = atan2(linSample.second, linSample.third) * (360 / Math.PI)
+        val yAngleRaw = atan2(linSample.first, linSample.third) * (360 / Math.PI)
+        val zAngleRaw = atan2(sqrt(linSample.first * linSample.first + linSample.second * linSample.second), linSample.third) * (180 / Math.PI)
+
+        val xGyroAngleRaw = atan2(gyroSample.second, gyroSample.third) * (360 / Math.PI)
+        val yGyroAngleRaw = atan2(gyroSample.first, gyroSample.third) * (360 / Math.PI)
+        val zGyroAngleRaw = atan2(sqrt(gyroSample.first * gyroSample.first + gyroSample.second * gyroSample.second), gyroSample.third) * (180 / Math.PI)
+
+        val xAngle = filterSensorFusion(xAngleRaw,xGyroAngleRaw)
+        val yAngle = filterSensorFusion(yAngleRaw,yGyroAngleRaw)
+        val zAngle = filterSensorFusion(zAngleRaw,zGyroAngleRaw)
+
+        return Triple(xAngle.toFloat(), yAngle.toFloat(), zAngle.toFloat())
+    }
+
+    private fun filterEWMA(value:Double,prevValue:Double):Double{
+        return alpha * value + (1 - alpha) * prevValue
+    }
+    private fun filterSensorFusion(x1:Double,x2:Double):Double{
+        return alpha * x1 + (1 - alpha) * x2
+    }
+}
